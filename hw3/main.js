978a87029672b9a4bb635a8e375f06c28987d0fc
Ivan <pig1995z@gmailcom>
2020-07-24 03:15:16 +0300

    add count

--- hw3/main.js
+++ hw3/main.js
@@ -59,3 +59,29 @@ function podatok(pod, widsotok) {
 document.write(
   `<b>Податок від 1000 грн буде </b> </br> ${podatok(1000, 19.5)} грн </br>`
 );
+/////Створити функцію, яка повертає випадкове ціле число в діапазоні від N до M. Приклад: getRandomNumber(1, 10) -> 5
+
+function randomNumb(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Включно з мінімальним та виключаючи максимальне значення
+}
+
+document.write(`<b>Випадкове число </b> </br> ${randomNumb(1, 10)}  </br>`);
+
+///
+
+function count(str, symb) {
+   // var symb = "о", str = "Астрономия это наука о небесных объектах";
+   var str = str.toLowerCase();
+      for (var count = -1, index = 0; index != -1; count++, index = str.indexOf(symb, index + 1));
+    
+      return count;
+   
+    
+   // document.writeln(count(str, symb)) // 5
+}
+ 
+document.write(
+  `<b>Символ  а  повторяется    раз.  </b> </br> ${count( 'коли я був малий то рахував букви ОООООО', 'о')}  </br>`
+);
