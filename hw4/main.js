3f5eb61be5080829d6f03a88daebd8ae96ae69e4
Ivan <pig1995z@gmailcom>
2020-08-01 00:37:46 +0300

    hw 4 massive

--- hw4/main.js
+++ hw4/main.js
@@ -12,26 +12,67 @@ const marks = [4, 5, 5, 3, 4, 5];
 // const teachers = ["Ivan", "Roman", "Andriy"];
 
 // // console.log(numbers);
-
+// debugger
 
 function getPairs(arr) {
     let onePairs = [];
     let twoPairs = [];
     let threPairs = [];
+    let sumMassive = [];
     if (arr) {  
         
         onePairs.push(arr[0], arr[2]);
         twoPairs.push(arr[1], arr[3]);
          threPairs.push(arr[4], arr[5]);
+
+         sumMassive.push(onePairs, twoPairs, threPairs);
+        } else {
+            return  console.log(`Error!`)
         }
 
-        return console.log(onePairs, twoPairs, threPairs);
+        return console.log(sumMassive);
   }
 
  getPairs(students) 
-// teachers.pop();
+//////////////
+ 
+ function getPairsAndThems(arr, arr2) {
+    let onePairs = [];
+    let twoPairs = [];
+    let threPairs = [];
+    let sumMassive = [];
 
+    if (arr) {    
+        onePairs.push(arr[0], arr[2], arr2[0]);
+        twoPairs.push(arr[1], arr[3], arr2[1])
+         threPairs.push(arr[4], arr[5], arr2[2])
+         sumMassive.push(onePairs, twoPairs, threPairs);
+        } else {
+            return  console.log(`Error!`)
+        }
 
+ 
+
+        return console.log(sumMassive);
+  }
+
+  getPairsAndThems(students, themes) 
+ 
+
+// запушити ТРИ МАСИВА В МАСИВ! function getPairs(arr) {
+//     let onePairs = [];
+   
+//     if (arr) {  
+        
+//         onePairs.push(arr[0], arr[2]);
+//         onePairs.push(arr[1], arr[3]);
+//         onePairs.push(arr[4], arr[5]);
+//         }
+
+//         return console.log(onePairs );
+//   }
+
+//  getPairs(students) 
 // console.log(teachers);
  
 // teachers.push();
