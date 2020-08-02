0d1527ae0bc18c4d92882a66ce449d3d994c5b25
Ivan <pig1995z@gmailcom>
2020-08-03 01:57:37 +0300

    filter

--- hw5/main.js
+++ hw5/main.js
@@ -18,31 +18,45 @@ console.log(getRandomArray(10, 2, 100));
 function average() {
   let sum = 0;
 
-  for (index = 0; index < arguments[index]; index++) sum = sum + arguments[index];
+  for (index = 0; index < arguments[index]; index++)
+    sum = sum + arguments[index];
 
   sum = sum / arguments.length;
 
   return sum;
 }
 
-console.log( "середнє арифметичне: ", average(10, 100, 10001, 15, 33));
+console.log("середнє арифметичне: ", average(10, 100, 10001, 15, 33));
 
-
-///MEDIAN
+///MEDIAN --- 4
 
 function calcMedian(...ar1) {
-
   let half = Math.floor(ar1.length / 2);
-  
-  ar1.sort(function(a, b) { return a - b;});
-  
+
+  ar1.sort(function (a, b) {
+    return a - b;
+  });
+
   if (ar1.length % 2) {
     return ar1[half];
   } else {
-    return (ar1[half-1] + ar1[half]) / 2.0;
+    return (ar1[half - 1] + ar1[half]) / 2.0;
   }
 }
 
-console.log('Mediana',  calcMedian( 6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+console.log(
+  "Mediana",
+  calcMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)
+);
+
+//Filter --- 5
+
+function filterEvenNumbers(...number) {
+  return number.filter((el) => el % 2);
+}
+
+console.log("Парні числа пропустити ", filterEvenNumbers(1, 2, 3, 4, 5, 6));
 
 //
+
+
