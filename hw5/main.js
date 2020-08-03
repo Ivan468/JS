a43813e260cb3c746ae78df41d95103406c0f147
Ivan <pig1995z@gmailcom>
2020-08-03 03:02:53 +0300

    document.write

--- hw5/main.js
+++ hw5/main.js
@@ -8,10 +8,10 @@ let getRandomArray = (length, min, max) => {
     max = Math.floor(max);
     arrRandom.push(Math.floor(Math.random() * (max - min)) + min);
   }
-  return arrRandom;
+  return arrRandom.join(", ");
 };
 
-console.log(getRandomArray(10, 2, 100));
+document.write("Рандомне число:  <b> ", getRandomArray(10, 2, 100), "  </b> <hr> <br>");
 
 // zavdannya 3
 
@@ -26,7 +26,11 @@ function average() {
   return sum;
 }
 
-console.log("середнє арифметичне: ", average(10, 100, 10001, 15, 33));
+document.write(
+  "середнє арифметичне: із 10, 100, 10001, 15, 33 буде:  <b>",
+  average(10, 100, 10001, 15, 33),
+  " </b> <hr> <br>"
+);
 
 ///MEDIAN --- 4
 
@@ -44,34 +48,44 @@ function calcMedian(...ar1) {
   }
 }
 
-console.log(
-  "Mediana",
-  calcMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2)
+document.write(
+  "Медіана із 6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2 буде: <b>",
+  calcMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2),
+  "</b><hr> <br>"
 );
-
-//Filter --- 5
+ 
+//Filter --- 5 arrRandom.join(", ")
 
 function filterEvenNumbers(...number) {
-  return number.filter((el) => el % 2);
+  return number.filter((el) => el % 2).join(", ");
 }
 
-console.log("Парні числа пропустити ", filterEvenNumbers(1, 2, 3, 4, 5, 6));
+document.write(
+  "Парні числа пропустити із  1, 2, 3, 4, 5, 6 буде: <b>",
+  filterEvenNumbers(1, 2, 3, 4, 5, 6),
+  "</b> <hr> <br>"
+);
 
 //Filter --- 6 - Більше за нуль
 
 function countPositiveNumbers(...number) {
-   a =  number.filter((el) => el > 0);
-     return a.length;
-  }
+  a = number.filter((el) => el > 0);
+  return a.length;
+}
 
-console.log("Кількість чисел ", countPositiveNumbers(1, -2, 3, -4, -5, 6)  );
+document.write(
+  "Кількість чисел більших за 0 серед 1, -2, 3, -4, -5, 6 буде <b>",
+  countPositiveNumbers(1, -2, 3, -4, -5, 6),
+  "</b> <hr> <br>"
+);
 //Filter --- 7 - ділення Фільтр тільки тих що діляться на 5
 
- 
-
-
 function getDividedByFive(...number) {
-    return number.filter(num => num % 5 === 0)
- }
+  return number.filter((num) => num % 5 === 0).join(", ");
+}
 
-console.log ("Фільтр тільки тих що діляться на 5", getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+document.write(
+  "Фільтр тільки тих що діляться на 5: <b>",
+  getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2),
+  "</b><br><hr> "
+);
