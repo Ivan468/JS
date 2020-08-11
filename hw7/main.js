79f9c571c332f222934eec1c09f5e3801dc2b340
Ivan <pig1995z@gmailcom>
2020-08-11 23:55:41 +0300

    2 tasks

--- hw7/main.js
+++ hw7/main.js
@@ -8,20 +8,29 @@ const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 // яка рахує скільки податків ви заплатите як IT-спеціаліст в якійсь з країн. 
 // Функція повинна викликатись через call та працювати з даними через this
 
+function getMyTaxes ( salary) {
+    return   ( salary*this.tax  )
+}
 
-function getMyTaxes (params) {
+document.write ( `Якщо зарплата буде 2000 то ІТ працівник отримає : <b>
+   ${getMyTaxes.call(ukraine, 2000)} податку</b> <br>`
+)
+
+//2
+
+function getMiddleTaxes  (params) {
     return   ( this.tax*this.middleSalary  )
 }
 
 document.write ( `ІТ Спеціаліст в Україні заплатить : <b>
-   ${getMyTaxes.call(ukraine)} податку</b> <br>`
+   ${getMiddleTaxes.call(ukraine)} податку</b> <br>`
 )
 
 document.write ( ` ІТ Спеціаліст в Латвії заплатить : <b>
-   ${getMyTaxes.call(latvia)} податку </b> <br>`
+   ${getMiddleTaxes.call(latvia)} податку </b> <br>`
 )
  
 document.write ( `ІТ Спеціаліст в Литві заплатить : <b>
-   ${getMyTaxes.call(litva)} податку</b> <br>`
+   ${getMiddleTaxes.call(litva)} податку</b> <br>`
 )
 
