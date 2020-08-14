ed5c05fbeac2567bcad628b2b6e2aaf91345cc8d
Ivan <pig1995z@gmailcom>
2020-08-14 22:59:14 +0300

    8 hw

---
+++ hw8/main.js
@@ -0,0 +1,24 @@
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+
+class Student {
+  constructor(fullName, university, course) {
+    this.university = university;
+    this.fullName = fullName;
+    this.course = course;
+  }
+}
+
+const student = new Student(`Petrushchack Ivan`, `KOTA`, `6`);
+
+console.log("student", student);
+
+
+ 
