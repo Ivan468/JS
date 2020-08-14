770b39e70bcaf7ce8db2c0575b9a56173189eb78
Ivan <pig1995z@gmailcom>
2020-08-15 00:10:31 +0300

    getter and seter

--- hw8/main.js
+++ hw8/main.js
@@ -19,11 +19,25 @@ class Student {
 
     return  "Студент " + this.course + 'го курсу ' + this.university + " " + this.fullName  ;
   }
+
+//   /**
+//      * @param {number[]} marksStudents
+//      */
+  set marks(marksStudents) {             //  встановлюємо оцінки
+    return  this.marksStudents = marksStudents;
+  }
+
+  get studMark() {         //   виводить на консоль оцінки
+ 
+    return  this.marksStudents ;
+  }
 }
 
 const student = new Student(`Петрущак Іван`, `Києвської православної богословської академії`, `6`);
 
 console.log(  student.getInfo());
 
+student.marks = [5, 4, 4, 5]; //   встановлюємо оцінки за допомогою сетера
 
- 
+
+console.log( "Оцінки студента", student.studMark);  //   виводимо оцінки за допомогою гетера
