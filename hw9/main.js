d11555b564a75b5d83b959af4242b0f18b01cf30
Ivan <pig1995z@gmailcom>
2020-08-18 01:11:47 +0300

    interval

--- hw9/main.js
+++ hw9/main.js
@@ -1,40 +1,17 @@
- function getRandomColor() {
+function getRandomColor() {
   return "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
-};
-
- 
-
-
-//  function color() {
-//   const box = document.createElement(`div`);
-//   box.style.background = getRandomColor();
-//  }
-
-// function setRandomColor() {
-//   document.body.children[0].style.background = getRandomColor();
-// }
+}
 const BOX = () => {
   const box = document.createElement(`div`);
-box.classList.add(`color`);
-box.style.height = `50px`;
-box.style.width = `50px`;
-// let a = setInterval(getRandomColor, 1000);
-box.style.background = getRandomColor() ;
+  box.classList.add(`color`);
+  box.style.height = `50px`;
+  box.style.width = `50px`;
+  setInterval(() => {box.style.background = getRandomColor();}, 1000)
+  document.body.children[0].insertAdjacentElement(`afterbegin`, box);
+};
 
-// document.body.before(box);
-// const flex = document.body.getElementsByClassName(`.flex`);
-document.body.children[0].insertAdjacentElement(`afterbegin`, box)
-}
-
-
-
-BOX(); BOX(); BOX();BOX();BOX();BOX();BOX();BOX();BOX();BOX();
-BOX(); BOX(); BOX();BOX();BOX();BOX();BOX();BOX();BOX();BOX();
 BOX();BOX();BOX();BOX();BOX();
-
-
-function generateBlocksInterval()  {
-  setInterval(getRandomColor, 1000);
-}
-
-generateBlocksInterval() 
+BOX();BOX();BOX();BOX();BOX();
+BOX();BOX();BOX();BOX();BOX();
+BOX();BOX();BOX();BOX();BOX();
+BOX();BOX();BOX();BOX();BOX();
