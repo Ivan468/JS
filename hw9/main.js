21b76c3e6d45a06adaac30f6904f8af202398fef
Ivan <pig1995z@gmailcom>
2020-08-18 00:40:39 +0300

    fix hw

--- hw9/main.js
+++ hw9/main.js
@@ -1,10 +1,33 @@
-function getRandomColor() {
-    
-    return  '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
-  }
-  
-   
-  
-  function setRandomColor() {
-    document.body.children[0].style.background = getRandomColor();
-  }
+ function getRandomColor() {
+  return "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+};
+
+ 
+
+
+//  function color() {
+//   const box = document.createElement(`div`);
+//   box.style.background = getRandomColor();
+//  }
+
+// function setRandomColor() {
+//   document.body.children[0].style.background = getRandomColor();
+// }
+const BOX = () => {
+  const box = document.createElement(`div`);
+box.classList.add(`color`);
+box.style.height = `50px`;
+box.style.width = `50px`;
+// let a = setInterval(getRandomColor, 1000);
+box.style.background = getRandomColor() ;
+
+// document.body.before(box);
+// const flex = document.body.getElementsByClassName(`.flex`);
+document.body.children[0].insertAdjacentElement(`afterbegin`, box)
+}
+
+
+
+BOX(); BOX(); BOX();BOX();BOX();BOX();BOX();BOX();BOX();BOX();
+BOX(); BOX(); BOX();BOX();BOX();BOX();BOX();BOX();BOX();BOX();
+BOX();BOX();BOX();BOX();BOX();
