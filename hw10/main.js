5872f8829af248643d2fa8f31a975e112a0da389
Ivan <pig1995z@gmailcom>
2020-08-22 19:19:55 +0300

    finish

--- hw10/main.js
+++ hw10/main.js
@@ -1,8 +1,18 @@
-window.addEventListener('keydown', function (e){
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  //   console.log("audio", key);
+  if (!audio) return; // stop function
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add(`playing`);
+});
 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    // console.log("audio", audio)
-    if (!audio) return // stop function
-    audio.currentTime = 0;
-    audio.play();
-})
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+  //   console.log(e.propertyName);
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
