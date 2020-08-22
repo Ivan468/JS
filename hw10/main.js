801b48c70776915da5351f2ad6339b8814df21e9
Ivan <pig1995z@gmailcom>
2020-08-22 21:05:19 +0300

    fix click

--- hw10/main.js
+++ hw10/main.js
@@ -16,3 +16,45 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+const kbd = document.querySelectorAll("kbd");
+
+
+keys.forEach((key) =>
+  key.addEventListener("click", function (e) {
+    
+  
+    const k = e.target.dataset.key;
+    const audio = document.querySelector(`audio[data-key="${k}"]`);
+
+
+
+    if (!audio)  return;  
+    audio.currentTime = 0;
+    audio.play();
+ 
+     
+  })
+);
+
+keys.forEach((kbd) =>
+kbd.addEventListener("click", function (e) {
+   
+  
+    const k = e.path[1].attributes[0].value;
+    const audio = document.querySelector(`audio[data-key="${k}"]`);
+
+
+    if (!audio)  return;  
+      audio.currentTime = 0;
+      audio.play();
+
+     
+  })
+);
+
+// NEW CODE
+// document.querySelector('.key').addEventListener('click', function (event){
+// console.log('click', event.target);
+
+// });
