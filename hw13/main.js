44905517cf2e58aa09df472697a8504fe0805b1d
Ivan <pig1995z@gmailcom>
2020-09-01 23:40:31 +0300

    add advence

--- hw13/main.js
+++ hw13/main.js
@@ -8,8 +8,8 @@ const myGen = gen();
 
 const kbd = document.querySelector(".btn");
 const ID = document.querySelector(`.id`);
-const upText = document.querySelector(`.upText`)
-const downText = document.querySelector(`.downText`)
+const upText = document.querySelector(`.upText`);
+const downText = document.querySelector(`.downText`);
 
 kbd.addEventListener("click", function (e) {
   return (ID.innerHTML = myGen.next().value);
@@ -23,39 +23,36 @@ downText.addEventListener("click", function (e) {
   return renderFontDown();
 });
 
-
 function* FontGenerator(value) {
-  let a;
-  for(let i = 0; i < Infinity ; i++) {
-      a = yield value;
-      if(a === 'up') {
-          value += 2;
-
-      } else if (a === 'down') {
-          value -= 2;
-      }
+  let a ;
+  for (let i = 0; i < Infinity; i++) {
+    if (a === "up") {
+      value += 2;
+    } else if (a === "down") {
+      value -= 2;
+    }
+    a = yield value;
   }
 }
 const fontGenerator = FontGenerator(14);
 
-
-
+fontGenerator.next();
 
 function renderFontUp() {
-  document.querySelector(".text").style.fontSize = `${fontGenerator.next('up').value}px`;
-
+  document.querySelector(".text").style.fontSize = `${
+    fontGenerator.next("up").value
+  }px`;
 }
 function renderFontDown() {
- document.querySelector(".text").style.fontSize = `${fontGenerator.next('down').value}px`;
-
+  document.querySelector(".text").style.fontSize = `${
+    fontGenerator.next("down").value
+  }px`;
 }
-
-
 
 // function idTwo() {
 //    let a = myGen.next().value;
 
-//    let b = `ID-` + a; 
+//    let b = `ID-` + a;
 //    return b;
 // }
 
